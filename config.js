fde43fb89401b128e3d3606166f64bcd53833902
SuperPartyByAI <superpartybyai@gmail.com>
2025-12-03 00:50:08 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+/**
+ * SUPERPARTY CONFIG
+ * Modifică doar URL-ul aici când faci deployment nou
+ */
+
+const SUPERPARTY_CONFIG = {
+    BACKEND_URL: 'https://script.google.com/macros/s/AKfycbzhOP0u8a6jBJEpFMth7fltpC069IeUKn1OMJKHI8tA2Rg5VWuNTpI7oJRy5Lv6SZFr/exec',
+    
+    // Parole admin/GM
+    ADMIN_PASSWORD: 'Adminandrei209512!',
+    GM_PASSWORD: 'Gmandrei209512!',
+    
+    // Versiune
+    VERSION: '5.0'
+};
+
+console.log('✅ SuperParty Config loaded - Backend:', SUPERPARTY_CONFIG.BACKEND_URL);
